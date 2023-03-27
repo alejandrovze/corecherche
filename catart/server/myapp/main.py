721e489cd878b6f07fb973bcbcbc6ff9c07edc10
Théo Jourdan <jourdan.theo@protonmail.com>
2023-03-27 17:52:42 +0200

    Two features: merge two sounds, move dots

--- catart/server/myapp/main.py
+++ catart/server/myapp/main.py
@@ -1,7 +1,7 @@
 # myapp.py
 
 from bokeh.layouts import column, row
-from bokeh.models import ColumnDataSource, CustomJS, Slider, Div, Button, Paragraph
+from bokeh.models import ColumnDataSource, CustomJS, Slider, Div, Button, Paragraph, PointDrawTool
 from bokeh.plotting import figure, output_file, show, curdoc
 from bokeh import events
 from bokeh.themes import built_in_themes, Theme
@@ -20,6 +20,8 @@ import io
 from bokeh.models.widgets import FileInput
 import os
 import time
+from sklearn import preprocessing
+
 # PLAY WITH A NEW SONG 
 splitNewSong = True
 ######################
@@ -46,27 +48,42 @@ class SplitWavAudioMubin():
     def get_duration(self):
         return self.audio.duration_seconds
     
-    def single_split(self, from_min, to_min, split_filename):
-        t1 = from_min  * 1000
-        t2 = to_min  * 1000
+    def single_split(self, from_min, to_min, split_filename, diviseur):
+        t1 = from_min  * 1000 // diviseur
+        t2 = to_min  * 1000 // diviseur
         split_audio = self.audio[t1:t2]
         #os.remove(self.finalFolder + file_changment + split_filename)
         split_audio.export(self.finalFolder +split_filename, format="wav")
         
-    def multiple_split(self, sec_per_split):
+    def single_split_milli(self, split_filename, split_start):
+
+        split_audio = self.audio[split_start:]
+        #os.remove(self.finalFolder + file_changment + split_filename)
+        split_audio.export(self.finalFolder +split_filename, format="wav")
+        
+    def multiple_split(self, sec_per_split, diviseur):
         total_mins = math.ceil(self.get_duration())
         for i in range(0, total_mins, sec_per_split):
             split_fn = str(i) + '_' + self.filename
             self.arrayName.append(split_fn)
-            self.single_split(i, i+sec_per_split, split_fn)
+            self.single_split(i, i+sec_per_split, split_fn, diviseur)
             print(str(i) + ' Done')
             if i == total_mins - sec_per_split:
-                print('All splited successfully')      
+                print('All splited successfully')  
+                
+    def multiple_split_milli(self, milli_time):
+        total_mins = math.ceil(self.get_duration()) * 1000
+        for i in range(0, total_mins, milli_time):
+            split_fn = str(i) + '_' + self.filename
+            self.arrayName.append(split_fn)
+            self.single_split_milli (split_fn, i)
+        print('Split Done')
+
                 
 if splitNewSong:
     audioAdress = 'myapp/static/split/'
     split_wav = SplitWavAudioMubin('myapp/static/', 'drum_sega1.wav', audioAdress)
-    split_wav.multiple_split(sec_per_split=1)
+    split_wav.multiple_split_milli(milli_time=200)
     twodtable = [np.array(['Filename', 'FondFreq', 'Amplitude'])]
     for segSound in split_wav.arrayName:  
         y, sr = librosa.load(audioAdress + segSound)
@@ -82,30 +99,46 @@ if splitNewSong:
         
 def set_carto(adresse):
     split_wav = SplitWavAudioMubin(audioAdress, adresse, audioAdress)
-    split_wav.multiple_split(sec_per_split=1)
+    split_wav.multiple_split_milli(milli_time=700)
     twodtable = [np.array(['Filename', 'FondFreq', 'Amplitude'])]
+    
+    all_meanCentroid = []
+    all_meanRMS = []
+    
     for segSound in split_wav.arrayName:  
         y, sr = librosa.load(audioAdress + segSound)
         cent = librosa.feature.spectral_centroid(y=y, sr=sr)
         meanCentroid = np.mean(cent)
-        
         rms = librosa.feature.rms(y=y)
         meanRMS = np.mean(rms)
-   
         
-        twodtable.append(np.array([segSound, meanCentroid, meanRMS]))
+        all_meanCentroid.append(meanCentroid)
+        all_meanRMS.append(meanRMS)
+        
+        #twodtable.append(np.array([segSound, meanCentroid, meanRMS]))
+
+    all_meanCentroid = preprocessing.normalize([all_meanCentroid])[0]
+    all_meanRMS = preprocessing.normalize([all_meanRMS])[0]
+    
+    for i in range(len(all_meanCentroid)):
+        twodtable.append(np.array([split_wav.arrayName[i], all_meanCentroid[i], all_meanRMS[i]]))
+    
     np.savetxt(audioAdress + 'scores_combined.txt', np.array(twodtable),fmt='%s', delimiter = "\t")
     
 
-file_input = FileInput(accept="audio")
-
-    
+file_input = FileInput(accept="audio")    
 dataFile = pd.read_csv(audioAdress + 'scores_combined.txt', sep="\t")
 nameSound = dataFile.to_numpy()[:, 0]
 x = dataFile.to_numpy()[:, 1]
 y = dataFile.to_numpy()[:, 2]
 state = ["False"]*len(x)
 color= ["red"]*len(x)
+
+nameSound = []
+x = []
+y = []
+state = []
+color= []
 
 def upload_song(attr,old,new):
     mytime = time.time()
@@ -125,13 +158,51 @@ def upload_song(attr,old,new):
     source2.data = {'x':[], 'y':[], 'color':[], 'state':[], 'nameSound':[]}
     source2.data = {'x':x, 'y':y, 'color':color, 'state':state, 'nameSound':nameSound}  
     
+def upload_song_add(attr,old,new):
+    mytime = time.time()
+    decoded = base64.b64decode(new)
+    #os.remove(audioAdress+ 'temp.wav')
+    wav_file = open(audioAdress + str(mytime) +'temp.wav', "wb")
+    wav_file.write(decoded)
+    
+    set_carto(str(mytime)+'temp.wav')
+    
+    dataFile = pd.read_csv(audioAdress + 'scores_combined.txt', sep="\t")
+    nameSound = dataFile.to_numpy()[:, 0]
+    x = dataFile.to_numpy()[:, 1]
+    y = dataFile.to_numpy()[:, 2]
+    state = ["False"]*len(x)
+    color= ["green"]*len(x)
+    
+    source2.data['x'] = np.concatenate((source2.data['x'], x))
+    source2.data['y'] = np.concatenate((source2.data['y'], y))
+    source2.data['color'] = np.concatenate((source2.data['color'], color))
+    source2.data['state'] = np.concatenate((source2.data['state'], state))
+    source2.data['nameSound'] = np.concatenate((source2.data['nameSound'], nameSound))
+    
 file_input = FileInput(accept="audio")
-file_input.on_change('value', upload_song)
+file_input.on_change('value', upload_song_add)
 
 source2 = ColumnDataSource(data={'x':x, 'y':y, 'color':color, 'state':state, 'nameSound':nameSound})
 source = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
 
-p = figure(width=1050, height=750, tools="pan,wheel_zoom,zoom_in,zoom_out,reset", title='PyTart (beta)')
+
+p = figure(width=1050, height=750, tools="zoom_in,zoom_out,reset", title='PyTart (beta)')
+
+
+#source = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
+sr = p.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='#D2CC1A', alpha=1, line_width=2, source=source, )
+    
+cr = p.circle('x', 'y', fill_color='color', size=5, alpha=1, hover_color='#ED553B', hover_alpha=1.0, source=source2)
+glyph = cr.glyph
+glyph.size = 20
+glyph.fill_alpha = 0.8
+glyph.line_color = "#ED553B"
+glyph.line_width = 2
+
+point_tool = PointDrawTool(renderers=[cr])
+p.add_tools(point_tool)
+p.toolbar.active_tap = point_tool
 
 p.xaxis.major_tick_line_color = None  # turn off x-axis major ticks
 p.xaxis.minor_tick_line_color = None  # turn off x-axis minor ticks
@@ -162,15 +233,7 @@ div = Div(width=400, height=p.height, height_policy="fixed")
 button = Button(label="Button", button_type="success")
 layout = row(p, column(file_input, vol_slider,len_slider,paraDelay,delay_slider1,delay_slider2,delay_slider3,paraPBrate,PBrate_slider1,paraRev,rev_slider1,rev_slider2,paraTrem,trem_slider1,trem_slider2,paraLp,lp_slider1))
 
-#source = ColumnDataSource({'x0': [], 'y0': [], 'x1': [], 'y1': []})
-sr = p.segment(x0='x0', y0='y0', x1='x1', y1='y1', color='#D2CC1A', alpha=1, line_width=3, source=source, )
-    
-cr = p.circle('x', 'y', fill_color='color', size=20, alpha=1, hover_color='#ED553B', hover_alpha=1.0, source=source2)
-glyph = cr.glyph
-glyph.size = 60
-glyph.fill_alpha = 0.8
-glyph.line_color = "#ED553B"
-glyph.line_width = 2
+
 
 point_attributes = ['x', 'y', 'sx', 'sy'] 
 
@@ -179,14 +242,14 @@ code = """
 const attrs = %s;
 const data = {'x0': [], 'y0': [], 'x1': [], 'y1': []};
 
-const x_cursor = Number(cb_obj[attrs[0]]).toFixed(2);
-const y_cursor = Number(cb_obj[attrs[1]]).toFixed(2);
+const x_cursor = Number(cb_obj[attrs[0]]).toFixed(10);
+const y_cursor = Number(cb_obj[attrs[1]]).toFixed(10);
 
 var dataCercle = source2.data;
 
 var localX = 0;
 var localY = 0;
-var localMini = Number.MAX_SAFE_INTEGER;
+var localMini = 1000.0;
 
 var finalX = 0;
 var finalY = 0;
@@ -210,8 +273,7 @@ const lpslider2 = lp_slider2.value;
 for (let i = 0; i < circle.data.x.length; i++) {
     localX = circle.data.x[i]
     localY = circle.data.y[i]
-    
-    var localMiniBis = Math.sqrt( Math.pow((x_cursor-localX), 2) + Math.pow((y_cursor-localY), 2) );
+    var localMiniBis = Math.hypot(Math.abs(x_cursor-localX), Math.abs(y_cursor-localY));
     
     if (localMiniBis < localMini){
         audioID = i;
